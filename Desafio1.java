cde5414f72267ff1a94a3b825caa029ad3d5fa73
JeronimoMorais <jcm@a.recife.ifpe.edu.br>
2021-01-25 03:03:12 -0300

    Primeiro commit.

---
+++ Desafio1.java
@@ -0,0 +1,36 @@
+// // Desafios
+
+// // Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+
+// // Entrada
+
+// // Você receberá 1 valor inteiro N, onde N > 0.
+
+// // Saída
+
+// // Exiba todos os números pares até o valor de entrada, sendo um em cada linha. 
+ 
+// // Exemplo de Entrada 	Exemplo de Saída
+
+// // 6 			2	
+// //				4
+// //				6
+
+// import java.io.IOException;
+// import java.util.Scanner;
+
+// public class Desafio1 {
+	
+//     public static void main(String[] args) throws IOException {
+    	
+//     	Scanner sc = new Scanner(System.in);
+    	
+//     	int N = sc.nextInt();
+    	
+    	
+//     	for (int i = 1 ; i <=  N; i++) {
+//     		if (i%2 == 0) System.out.println(i);
+//     	}
+//     }
+	
+// }
